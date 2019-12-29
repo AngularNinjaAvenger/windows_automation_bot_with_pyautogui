405503cf3598b07fd2d0a4c13589d49d30d21e36
osayi bishop <eguaborbishop1@gmail.com>
2019-12-29 09:30:46 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import pyautogui
+x = pyautogui
+# a = x.locateOnScreen("C:/Users/angular_nija_avenger/Documents/AUTOMATION/img.png")
+a = x.screenshot()
+print(a)
+# x.keyUp("alt")
+# x.press("f4")
+# x.keyDown("alt")
+# x.keyDown("win")
+# x.press("r")
+# x.keyUp("win")
+# x.press("enter")
+# x.click(496,192)
+# terminalCode = "code quickworkworkspace.code-workspace"
+# ul = "cd C:/Users/angular_nija_avenger/Documents/QuickWork/ye/src/Auth/Signup/container/Stage3"
+# x.typewrite(ul)
+# x.press("enter")
+# x.typewrite(terminalCode)
+# x.press("enter")
+# x.click(329,250)
+# x.rightClick(171,249)
+# x.click(286,350)
+# x.typewrite("yarn start")
+# #x.click(775,195)code quickworkworkspace.code-workspace
+# #x.typewrite(terminalCode)
+# #x.press("enter")
+# # x.typewrite("what popaing this is bishop here")
